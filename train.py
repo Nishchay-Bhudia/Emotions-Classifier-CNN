4e102c6c57eac7946b17144a629323f11d487f0a
Nishchay Bhudia <nishchaybhudia@gmail.com>
2026-03-05 17:29:57 +0000

    Add the training loop

--- train.py
+++ train.py
@@ -17,3 +17,37 @@ model =EmotionCNN().to(device)
 #   loss + Optimizer
 criterion =nn.CrossEntropyLoss()
 optimizer= optim.Adam(model.parameters(), lr=0.001)
+
+# training loop
+epochs= 10
+
+for epoch in range(epochs):
+    model.train()
+    running_loss =0.0
+    correct = 0
+    total = 0
+
+    for images, labels in train_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+
+        #forward pass
+        output =model(images)
+        loss = criterion(output, labels)
+
+        #Backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+        # Accuracy calculation
+        _, predicted = torch.max(output, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    accuracy = 100 *correct/ total
+    print(f"Epoch[{epoch+1}/{epochs}] "
+          f"Loss: {running_loss:.4f} "
+          f"Accuracy: {accuracy:.2f}%")
